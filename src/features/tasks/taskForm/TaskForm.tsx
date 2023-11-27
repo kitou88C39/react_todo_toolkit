e21c53cc2dae591477533d13da975fdf0bc8a093
kitou88C39 <kanfutrooper@icloud.com>
2023-11-27 17:47:23 +0900

    TaskForm.tsx 編集

--- src/features/tasks/taskForm/TaskForm.tsx
+++ src/features/tasks/taskForm/TaskForm.tsx
@@ -1,7 +1,14 @@
+import TextField from '@mui/material/TextField';
 import styles from './TaskForm.module.scss';
 
 const TaskForm: React.FC = () => {
-  return <div>TaskForm</div>;
+  return (
+    <div className={styles.root}>
+      <form className={styles.form} noValidate autoComplete='off'>
+        <TextField id='outlined-basic' label='New Task' variant='outlined' />
+      </form>
+    </div>
+  );
 };
 
 export default TaskForm;
