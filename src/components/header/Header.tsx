226ff9e0bf8a93e05175a32dcda51e3362f14e2c
kitou88C39 <kanfutrooper@icloud.com>
2023-11-27 17:07:19 +0900

    Header.tsx 編集

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,10 +6,10 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <div className={styles.root}>
-      <AppBar position='static'>
-        <Toolbar>
+      <AppBar position='static' className={styles.app_bar}>
+        <Toolbar className={styles.tool_bars}>
           <Typography variant='h6' className={styles.root}>
-            News
+            Redux Toolkit Todo
           </Typography>
         </Toolbar>
       </AppBar>
