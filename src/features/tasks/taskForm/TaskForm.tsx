e05bc8b5936ac89fde15a18607ab40af00e50f11
kitou88C39 <kanfutrooper@icloud.com>
2023-11-27 19:57:14 +0900

    App.tsx 編集

--- src/features/tasks/taskForm/TaskForm.tsx
+++ src/features/tasks/taskForm/TaskForm.tsx
@@ -1,11 +1,25 @@
+import { useForm } from 'react-hook-form';
 import TextField from '@mui/material/TextField';
 import styles from './TaskForm.module.scss';
 
+type Inputs = {
+  taskTitle: string;
+};
+
 const TaskForm: React.FC = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const handleCreate = (data: Inputs) => {
+    console.log(data);
+  };
   return (
     <div className={styles.root}>
       <form className={styles.form} noValidate autoComplete='off'>
-        <TextField id='outlined-basic' label='New Task' variant='outlined' />
+        <TextField
+          id='outlined-basic'
+          label='New Task'
+          variant='outlined'
+          className={styles.text_field}
+        />
       </form>
     </div>
   );
