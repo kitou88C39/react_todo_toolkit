f4dfdfcb7fd9d462d2085e8190238384e59f71bc
kitou88C39 <kanfutrooper@icloud.com>
2023-11-28 10:34:19 +0900

    TaskItem.module.scss 作成

--- src/features/tasks/taskItem/TaskItem.tsx
+++ src/features/tasks/taskItem/TaskItem.tsx
@@ -1,5 +1,27 @@
-const TaskItem = () => {
-  return <div>TaskItem</div>;
+import Checkbox from '@mui/material/Checkbox';
+import styles from './TaskItem.module.scss';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+interface PropTypes {
+  task: { id: number; title: string; completed: boolean };
+}
+
+const TaskItem: React.FC = ({ task }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>
+        <EventNoteIcon />
+        <div className={styles.title_text}>{task.title}</div>
+        <Checkbox
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TaskItem;
