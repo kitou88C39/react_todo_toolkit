e421736436df564a0e4899189c73a7f705c9fa4e
kitou88C39 <kanfutrooper@icloud.com>
2023-11-30 11:40:22 +0900

    taskSlice.ts 編集

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -33,9 +33,7 @@ export const taskSlice = createSlice({
 
 export const { createTask } = taskSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value;
+export const selectTask = (state: RootState): TaskState['tasks'] =>
+  state.task.tasks;
 
 export default taskSlice.reducer;
