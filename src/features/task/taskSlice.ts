35ad3ccb99466c5ea1bb1f7b121c8bb9d364b0d2
kitou88C39 <kanfutrooper@icloud.com>
2023-11-30 11:29:21 +0900

    taskSlice.ts 編集

--- src/features/task/taskSlice.ts
+++ src/features/task/taskSlice.ts
@@ -31,22 +31,11 @@ export const taskSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = taskSlice.actions;
+export const { createTask } = taskSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectCount = (state: RootState) => state.counter.value;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd =
-  (amount: number): AppThunk =>
-  (dispatch, getState) => {
-    const currentValue = selectCount(getState());
-    if (currentValue % 2 === 1) {
-      dispatch(incrementByAmount(amount));
-    }
-  };
 
 export default taskSlice.reducer;
